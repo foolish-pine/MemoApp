b6ba126e49225326a4a36b374ec514ce406273f9
foolish-pine <matsuda.takuya41@gmail.com>
2024-11-26 12:52:05 +0900

    refactor: extract as component

---
+++ src/app/memo/list.tsx
@@ -0,0 +1,28 @@
+import { StyleSheet, View } from "react-native";
+
+import CircleButton from "../../components/CircleButton";
+import Header from "../../components/Header";
+import MemoListItem from "../../components/MemoListItem";
+
+const Index = () => {
+  return (
+    <View style={styles.container}>
+      <Header />
+      <View>
+        <MemoListItem />
+        <MemoListItem />
+        <MemoListItem />
+      </View>
+      <CircleButton>＋</CircleButton>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+  },
+});
+
+export default Index;
---
+++ src/components/CircleButton.tsx
@@ -0,0 +1,47 @@
+import { ReactNode } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+interface Props {
+  children: ReactNode;
+}
+
+const CircleButton = ({ children }: Props) => {
+  return (
+    <View style={styles.circleButton}>
+      <Text style={styles.circleButtonLabel}>{children}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+  },
+  circleButton: {
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    backgroundColor: "#467FD3",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    right: 40,
+    bottom: 40,
+    shadowColor: "#000000",
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    shadowOffset: {
+      width: 0,
+      height: 8,
+    },
+    elevation: 8,
+  },
+  circleButtonLabel: {
+    color: "#ffffff",
+    fontSize: 40,
+    lineHeight: 48,
+  },
+});
+
+export default CircleButton;
